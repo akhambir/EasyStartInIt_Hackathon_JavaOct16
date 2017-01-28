28f76bec56ac498bcfd9fc0df8e82481d2f47e13
melnikovae87 <melnikovae87@gmail.com>
2017-01-28 17:39:21 +0200

    add registration controller/Service

--- src/main/java/com/easystartinit/java_oct_16/controller/UserController.java
+++ src/main/java/com/easystartinit/java_oct_16/controller/UserController.java
@@ -1,19 +1,22 @@
 package com.easystartinit.java_oct_16.controller;
 
-import com.easystartinit.java_oct_16.service.UserServiceImpl;
+import com.easystartinit.java_oct_16.model.User;
+import com.easystartinit.java_oct_16.service.interfaces.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class UserController {
 
     @Autowired
-    private UserServiceImpl userService;
+    private UserService userService;
 
-    @RequestMapping(value = "/login")
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
     public String getLoginPage(@RequestParam(value = "error", required = false) String error,
                                @RequestParam(value = "logout", required = false) String logout, Model model) {
 
@@ -28,5 +31,17 @@ public class UserController {
         }
 
         return "loginPage";
+    }
+
+    @RequestMapping(value = "/registration", method = RequestMethod.GET)
+    public String getRegistrationPage(Model model) {
+        model.addAttribute("newUser", new User());
+        return "registrationPage";
+    }
+
+    @RequestMapping(value = "/registration/addUser", method = RequestMethod.POST)
+    public String addNewUser(@ModelAttribute User user, Model model) {
+        model.addAttribute("newUser", userService.addUser(user));
+        return "homePage";
     }
 }
--- src/main/java/com/easystartinit/java_oct_16/service/UserServiceImpl.java
+++ src/main/java/com/easystartinit/java_oct_16/service/UserServiceImpl.java
@@ -1,8 +1,18 @@
 package com.easystartinit.java_oct_16.service;
 
+import com.easystartinit.java_oct_16.dao.interfaces.UserDao;
+import com.easystartinit.java_oct_16.model.User;
 import com.easystartinit.java_oct_16.service.interfaces.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserDao userDao;
+
+    public User addUser(User user) {
+        return userDao.create(user);
+    }
 }
