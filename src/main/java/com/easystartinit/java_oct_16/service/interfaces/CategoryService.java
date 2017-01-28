4876ef53a8e8ef6098e3410dbb3fe46785776970
Max <maxym.t28@gmail.com>
2017-01-29 00:08:28 +0200

    fixed commits

--- src/main/java/com/easystartinit/java_oct_16/service/interfaces/CategoryService.java
+++ src/main/java/com/easystartinit/java_oct_16/service/interfaces/CategoryService.java
@@ -9,11 +9,10 @@ public interface CategoryService {
 
     List<Category> getAll();
 
-<<<<<<< HEAD
     Category create(Category category);
-=======
+
     Category read(Long id);
 
     boolean saveImage(MultipartFile multipartFile, String fileName);
->>>>>>> 613ae085f4f55887a41f31cd653d48429f24f8bd
+
 }
