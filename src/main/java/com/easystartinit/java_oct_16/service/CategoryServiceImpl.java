28b2099079b0d6c6ce4cf2b21fcbc506fa1c0b45
melnikovae87 <melnikovae87@gmail.com>
2017-01-28 23:57:02 +0200

    add some updates

--- src/main/java/com/easystartinit/java_oct_16/service/CategoryServiceImpl.java
+++ src/main/java/com/easystartinit/java_oct_16/service/CategoryServiceImpl.java
@@ -3,8 +3,10 @@ package com.easystartinit.java_oct_16.service;
 import com.easystartinit.java_oct_16.dao.interfaces.CategoryDao;
 import com.easystartinit.java_oct_16.model.Category;
 import com.easystartinit.java_oct_16.service.interfaces.CategoryService;
+import com.easystartinit.java_oct_16.service.interfaces.ImageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -14,12 +16,25 @@ public class CategoryServiceImpl implements CategoryService {
     @Autowired
     private CategoryDao categoryDao;
 
+    @Autowired
+    private ImageService imageService;
+
     public List<Category> getAll() {
         return categoryDao.readAll(Category.class);
     }
 
+    @Override
     public Category create(Category category) {
         return categoryDao.create(category);
     }
 
+    @Override
+    public Category read(Long id) {
+        return categoryDao.read(Category.class, id);
+    }
+
+    @Override
+    public boolean saveImage(MultipartFile multipartFile, String fileName) {
+        return imageService.saveImage(multipartFile, "/home/maxt28/Desktop/externalImages/"+fileName);
+    }
 }
