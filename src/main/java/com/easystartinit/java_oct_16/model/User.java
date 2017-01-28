32f6ed4b54b025a04258ab5d612d4ad33b5d9ce1
Max <maxym.t28@gmail.com>
2017-01-28 14:05:52 +0200

    Configured entities using hibernate

--- src/main/java/com/easystartinit/java_oct_16/model/User.java
+++ src/main/java/com/easystartinit/java_oct_16/model/User.java
@@ -1,15 +1,66 @@
 package com.easystartinit.java_oct_16.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "users")
 public class User {
 
+    @Id
+    @Column(name = "username")
     private String username;
+
+    @Column(name = "password")
     private String password;
+
+    @Column(name = "email")
     private String email;
-    private Boolean enabled;
+
+    @Column(name = "enabled")
+    private Boolean enabled = true;
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "user")
     private Set<UserRoles> userRoles = new HashSet<UserRoles>(0);
 
+    public String getUsername() {
+        return username;
+    }
 
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Boolean getEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(Boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public Set<UserRoles> getUserRoles() {
+        return userRoles;
+    }
+
+    public void setUserRoles(Set<UserRoles> userRoles) {
+        this.userRoles = userRoles;
+    }
 }
